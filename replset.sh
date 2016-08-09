cf977e27d3944575116e30b0f9ff030a32a39327
xun <xun1991@me.com>
2016-08-09 15:35:44 +0800

    added configurator script to assist setting up mongo replset

---
+++ replset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mongo $MONGO_MASTER:27017/admin --eval "rs.initiate(\"{\"_id\": \"$MONGO_REPLSET_NAME\", \"members\": [{\"_id\": 0, \"host\": \"$MONGO_MASTER:27017\"}]}\");"
+
+hosts=$(echo $MONGO_SECONDARY | tr "," "\n")
+
+for hst in $hosts
+do
+  mongo $MONGO_MASTER:27017/admin --eval "rs.add(\"$hst:27017\")";
+done
